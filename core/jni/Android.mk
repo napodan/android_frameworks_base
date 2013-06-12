d603cc5b2a9a7b7f446ed1494c020ee80d4ff6d0
napodan <david.guenet@gmail.com>
2013-06-12 09:32:01 +0200

    Revert "use new libreboot method of rebooting"
    This reverts commit 5bbad605303799efbc21599702460c6c3203e945.

--- core/jni/Android.mk
+++ core/jni/Android.mk
@@ -202,7 +202,7 @@ LOCAL_SHARED_LIBRARIES := \
 	libjpeg \
 	libnfc_ndef
 
-LOCAL_STATIC_LIBRARIES := libmtphost libusbhost libreboot
+LOCAL_STATIC_LIBRARIES := libmtphost libusbhost
 
 ifeq ($(BOARD_HAVE_BLUETOOTH),true)
 LOCAL_C_INCLUDES += \
