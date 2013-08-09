b3ec47a53c4eefa4ed434d8999baa02627dd91cf
napodan <david.guenet@gmail.com>
2013-08-09 22:28:40 +0200

    Update to use some 4.3_r2.1 build files

--- cmds/am/Android.mk
+++ cmds/am/Android.mk
@@ -1,14 +1,15 @@
 # Copyright 2008 The Android Open Source Project
 #
 LOCAL_PATH:= $(call my-dir)
-include $(CLEAR_VARS)
 
+include $(CLEAR_VARS)
 LOCAL_SRC_FILES := $(call all-subdir-java-files)
 LOCAL_MODULE := am
 include $(BUILD_JAVA_LIBRARY)
 
 include $(CLEAR_VARS)
-ALL_PREBUILT += $(TARGET_OUT)/bin/am
-$(TARGET_OUT)/bin/am : $(LOCAL_PATH)/am | $(ACP)
-	$(transform-prebuilt-to-target)
-
+LOCAL_MODULE := am
+LOCAL_SRC_FILES := am
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_MODULE_TAGS := optional
+include $(BUILD_PREBUILT)
